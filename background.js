a4884c549848931e490b400d0ddb4ab51c656fcf
yokuny <felipe.vni@hotmail.com>
2020-12-20 17:43:41 -0300

    done

--- background.js
+++ background.js
@@ -1,26 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#osInfo').innerHTML = window.navigator.platform;
-    // let userBrowser, userInfo = navigator.userAgent;
-    // console.log(userInfo);
-    // if(userInfo.indexOf("Chrome") > -1) {
-    //     userBrowser = "Google Chrome";
-    // } else if (userInfo.indexOf("Safari") > -1) {
-    //     userBrowser = "Apple Safari";
-    // } else if (userInfo.indexOf("Opera") > -1) {
-    //     userBrowser = "Opera";
-    // } else if (userInfo.indexOf("Firefox") > -1) {
-    //     userBrowser = "Mozilla Firefox";
-    // } else if (userInfo.indexOf("MSIE") > -1) {
-    //     userBrowser = "Microsoft Internet Explorer";
-    // }
-    // document.getElementById('userNavigator').innerHTML = `navigator ${userBrowser}`;
-    // let noOsNavigatorVersion = userInfo.split(/[()]+/);
-    document.querySelector('#navigatorInfo').innerHTML = navigator.userAgent;
-    document.querySelector('#vendorInfo').innerHTML = navigator.vendor;
     document.querySelector('#userCPUClocks').innerHTML = `machine has ${navigator.hardwareConcurrency} CPU's cores`;
-    document.querySelector('#acessData').innerHTML = Date();
-    // const mouseMovePage = document.getElementById('mouseMovePage');
-    // const mouseMoveClient = document.getElementById('mouseMoveClient');
+    let userBrowser, userInfo = navigator.userAgent;
+    if(userInfo.indexOf("Chrome") > -1) {
+        userBrowser = "Google Chrome";
+    } else if (userInfo.indexOf("Safari") > -1) {
+        userBrowser = "Apple Safari";
+    } else if (userInfo.indexOf("Opera") > -1) {
+        userBrowser = "Opera";
+    } else if (userInfo.indexOf("Firefox") > -1) {
+        userBrowser = "Mozilla Firefox";
+    } else if (userInfo.indexOf("MSIE") > -1) {
+        userBrowser = "Microsoft Internet Explorer";
+    }
+    document.querySelector('#userNavigator').innerHTML = `navigator ${userBrowser}`;
+    document.querySelector('#vendorInfo').innerHTML = navigator.vendor;
+    document.querySelector('#navigatorInfo').innerHTML = navigator.userAgent;
+    function screenSize(){
+        document.getElementById('windowSize').innerHTML = `display resolution ${screen.width} x ${screen.height}`;
+        document.getElementById('contentSize').innerHTML = `display usage ${window.innerWidth} x ${window.innerHeight}`;
+    };
+    window.addEventListener('resize', function(){
+        screenSize();
+    });
+    screenSize();
+    var clicksCount = 1;
+    window.addEventListener('click', function(click) {
+        document.getElementById('mouseClick').innerHTML = `clicked at ( x: ${click.clientX} y: ${click.clientY} )`;
+        document.getElementById('mouseClicksCount').innerHTML = `have clicked ${clicksCount} times`;
+        clicksCount++;
+    });
     window.addEventListener('mousemove', function(position) {
         const mouse = {
             page: {
@@ -39,27 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
             return [`${type} ( x: ${mouse[type].x} y: ${mouse[type].y} )`];
         });
     });
-    // const mouseClick = document.getElementById('mouseClick');
-    var clicksCount = 1;
-    window.addEventListener('click', function(click) {
-        document.getElementById('mouseClick').innerHTML = `clicked at ( x: ${click.clientX} y: ${click.clientY} )`;
-        document.getElementById('mouseClicksCount').innerHTML = `have clicked ${clicksCount} times`;
-        clicksCount++;
-    });
-    // const windowSize = document.getElementById('windowSize');
-    // const contentSize = document.getElementById('contentSize');
-    function screenSize(){
-        // const windowWidth = window.innerWidth;
-        // const windowHeight = window.innerHeight;
-        // const screenWidth = screen.width;
-        // const screenHeight = screen.height;
-        document.getElementById('windowSize').innerHTML = `display resolution ${screen.width} x ${screen.height}`;
-        document.getElementById('contentSize').innerHTML = `display usage ${window.innerWidth} x ${window.innerHeight}`;
-    };
-    window.addEventListener('resize', function(){
-        screenSize();
-    });
-    screenSize();
+    document.querySelector('#acessData').innerHTML = Date();
     document.querySelector('#leftThePage').innerHTML = '';
     document.querySelector('#pageShowed').innerHTML = '';
 });
